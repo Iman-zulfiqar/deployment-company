e43a70824467fd04a3d091541e4961c587b4c674
iman zulfiqar <imanzulfiqar298@gmail.com>
2023-02-21 18:38:26 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+document.querySelector(".fa-bars").addEventListener("click", () => {
+  document.querySelector("#mob").style.right = "0px";
+  document.querySelector(".fa-xmark").style.display = "inline";
+  document.querySelector(".fa-bars").style.visibility = "hidden";
+  document.querySelector(".t").style.right = "10px";
+});
+document.querySelector(".fa-xmark").addEventListener("click", () => {
+  document.querySelector(".fa-xmark").style.display = "none";
+  // document.querySelector(".fa-bars").style.display = "block";
+  document.querySelector(".fa-bars").style.visibility = "visible";
+  // document.querySelector(".navbar").style.marginTop = "0px";
+  document.querySelector(".t").style.right = "-200px";
+  document.querySelector("#mob").style.right = "-200px";
+});
